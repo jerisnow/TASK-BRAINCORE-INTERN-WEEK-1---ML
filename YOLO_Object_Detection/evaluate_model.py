496352afb647ecb014b73387325f8bbf48ad8310
Aura <133016493+jerisnow@users.noreply.github.com>
2024-08-19 22:06:43 +0700

    Update evaluate_model.py

--- YOLO_Object_Detection/evaluate_model.py
+++ YOLO_Object_Detection/evaluate_model.py
@@ -1,1 +1,26 @@
+from ultralytics import YOLO
+import matplotlib.pyplot as plt
+
+
+model = YOLO('/content/best_model.pt')
+
+image_paths = [
+    '/content/test1.jpg',
+    '/content/test2.jpg',
+    '/content/test3.jpg',
+    '/content/test4.jpg',
+    '/content/test5.jpg'
+]
+
+for i, image_path in enumerate(image_paths):
+    results = model(image_path)
+    
+    results[0].show()
+    
+    img_with_boxes = results[0].plot() 
+    plt.figure(figsize=(10, 6)) 
+    plt.imshow(img_with_boxes)
+    plt.axis('off')
+    plt.title(f'Object Detection Result for Images{i+1}')
+    plt.show()
 
